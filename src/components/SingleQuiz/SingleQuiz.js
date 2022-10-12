7b4afb4e65b0a4b98ed0d615aecf828f32b150a1
zhoaib <zhoaibhassan@gmail.com>
2022-10-12 18:32:22 +0600

    correct answer

--- src/components/SingleQuiz/SingleQuiz.js
+++ src/components/SingleQuiz/SingleQuiz.js
@@ -3,15 +3,35 @@ import './SingleQuiz.css'
 
 const SingleQuiz = ({ singlequiz }) => {
 
-    const { options, question, id, correctAnswer, } = singlequiz
+    const { options, question, id, correctAnswer, } = singlequiz;
+
+
+    const btnAnsClick = (correctAnswer) => {
+        if (correctAnswer === correctAnswer) {
+            alert(correctAnswer)
+        }
+    }
+
+    const btnOptClick = (option) => {
+        if (option === correctAnswer && correctAnswer === correctAnswer) {
+            alert('Your Answer is Right!!!')
+        }
+        else {
+            alert('Wrong Answer :(')
+        }
+    }
+
     return (
         <div>
 
             <div className='ques-ans'>
+                <button onClick={() => btnAnsClick(correctAnswer)}>Answer</button>
+
                 <h3>{question.slice(3, -4)}</h3>
 
+
                 <div className='ans'>
-                    {options.map(option => <button key={id} className='option-btn'><input type="radio" />{option}</button>)}
+                    {options.map(option => <button onClick={() => btnOptClick(option)} key={id} className='option-btn'><input type="radio" />{option}</button>)}
                 </div>
             </div>
 
